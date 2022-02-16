d1696627d86fc7dba8916769c44c806187e05749
see-why <iyadicy@gmail.com>
2022-02-16 10:52:21 +0100

    Rswag files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'random', to: 'greetings#random'
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get 'random', to: 'greetings#random'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
