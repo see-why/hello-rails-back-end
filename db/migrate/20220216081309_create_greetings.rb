a7c59499bb0c23c1c0006079a58868e0b17330e3
see-why <iyadicy@gmail.com>
2022-02-16 09:32:52 +0100

    Migration

--- db/migrate/20220216081309_create_greetings.rb
+++ db/migrate/20220216081309_create_greetings.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
+# Greetings table
 class CreateGreetings < ActiveRecord::Migration[7.0]
   def change
     create_table :greetings do |t|
-
+      t.string :message
       t.timestamps
     end
   end
